17a20948b6998a9a2db7ee63ea954830de9ce514
dineshGargCreole <dinesh.garg@creolestudios.com>
2024-04-22 17:12:00 +0530

    add test ai review

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import Layout from "./components/layout";
-import Global from "./assets/styles/globalStyles";
+import React from 'react';
+import Layout from './components/layout';
+import Global from './assets/styles/globalStyles';
 
-import "./App.css";
+import './App.css';
 
 function App() {
+  console.log('helllloo');
   return (
-    <div className="App">
+    <div className='App'>
       <Global />
       <Layout>
         <p>This is a App</p>
